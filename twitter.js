b7a4bb09046194922475cf74274e9ebbb462dfd1
panforrest <guoqianp@gmail.com>
2016-11-24 00:20:25 -0500

    tweets by username or search

--- twitter.js
+++ twitter.js
@@ -3,24 +3,42 @@ var router = express.Router();
 var Twitter = require('twitter');
 
 router.get('/', function(req, res, next) {
-
-	var username = req.query.username
-
-	if (username == null) {
-		res.json({
-			confirmation: 'fail',
-			message: 'Please enter a username'
-		})
-
-		return
-	}
-
 	var client = new Twitter({
 	  consumer_key: 'eq09Tts3bLDQzGRf4a1VXDNGw',
 	  consumer_secret: 'boonuJfSQlL9W3tlCV2hR86v2JukscfxNJ9PWOVwRAmLkhp8wz',
 	  access_token_key: '158018675-xrS1KU3Q73OVtJZy2u6CyKhZE2UFpMwPcbCEthtL',
 	  access_token_secret: 'DAhuIjLQFp1FyK2QCtW2o0o9Stn7JqNDm2xatOK4owZdA'	  
 	});
+
+	var username = req.query.username
+
+	if (username == null) {
+		// res.json({
+		// 	confirmation: 'fail',
+		// 	message: 'Please enter a username'
+		// })
+
+		// return
+		var search = req.query.search
+		if (search == null){
+			res.json({
+				confirmation: 'fail',
+				message: 'Please enter a search term or username.'
+			})
+
+			return
+		}
+
+		//search
+		client.get('search/tweets', {q: search}, function(error, tweets, response) {
+		   //console.log(tweets);
+		   res.json(tweets)
+		});
+
+		return
+	}
+
+
 
 	var params = {screen_name: username};
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
