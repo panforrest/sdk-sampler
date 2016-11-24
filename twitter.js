106c29f0a3ce47b183adad270afd5595ca7d9466
panforrest <guoqianp@gmail.com>
2016-11-23 23:12:48 -0500

    twitterSDKintegration

---
+++ twitter.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var Twitter = require('twitter');
+
+router.get('/', function(req, res, next) {
+
+	var client = new Twitter({
+	  consumer_key: 'eq09Tts3bLDQzGRf4a1VXDNGw',
+	  consumer_secret: 'boonuJfSQlL9W3tlCV2hR86v2JukscfxNJ9PWOVwRAmLkhp8wz',
+	  access_token_key: '158018675-xrS1KU3Q73OVtJZy2u6CyKhZE2UFpMwPcbCEthtL',
+	  access_token_secret: 'DAhuIjLQFp1FyK2QCtW2o0o9Stn7JqNDm2xatOK4owZdA'	  
+	});
+
+	var params = {screen_name: 'nodejs'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    console.log(tweets);
+	  }
+
+	  res.json(tweets)
+	});
+
+
+});
+
+module.exports = router;
