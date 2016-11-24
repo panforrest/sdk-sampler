ddfc3ded83bd30bdc837a74d612c9e443207226c
panforrest <guoqianp@gmail.com>
2016-11-23 23:29:08 -0500

    pullTwitterUserTweets

--- twitter.js
+++ twitter.js
@@ -4,6 +4,17 @@ var Twitter = require('twitter');
 
 router.get('/', function(req, res, next) {
 
+	var username = req.query.username
+
+	if (username == null) {
+		res.json({
+			confirmation: 'fail',
+			message: 'Please enter a username'
+		})
+
+		return
+	}
+
 	var client = new Twitter({
 	  consumer_key: 'eq09Tts3bLDQzGRf4a1VXDNGw',
 	  consumer_secret: 'boonuJfSQlL9W3tlCV2hR86v2JukscfxNJ9PWOVwRAmLkhp8wz',
@@ -11,7 +22,7 @@ router.get('/', function(req, res, next) {
 	  access_token_secret: 'DAhuIjLQFp1FyK2QCtW2o0o9Stn7JqNDm2xatOK4owZdA'	  
 	});
 
-	var params = {screen_name: 'nodejs'};
+	var params = {screen_name: username};
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 	  if (!error) {
 	    console.log(tweets);
